a4962fed169d763d13ee0e3ac8e3270ad59840da
JUNE\june <kuc2477@gmail.com>
2017-02-27 16:00:37 +0900

    use tf.app.flags

--- implementations/GAN/models.py
+++ implementations/GAN/models.py
@@ -1,24 +1,39 @@
+import os.path
+import numpy as np
 import tensorflow as tf
 from tensorflow.contrib import slim
+from tensorflow.examples.tutorials.mnist import input_data
 from activations import lrelu
+import utils
 
 
 class DCGAN(object):
-    def __init__(self, z_size, image_size, lr=0.0002, beta1=0.5, sess=None,
+    def __init__(self, z_size, image_size, learning_rate=0.0002, beta1=0.5,
+                 iterations=5000, batch_size=128, generator_update_ratio=2,
+                 log_for_every=10, save_for_every=10,
                  sample_dir='./figures', model_dir='./models'):
+        # training related variables and hyperparameters
         self._initialzier = tf.truncated_normal_initializer(stddev=0.02)
+        self._iterations = iterations
+        self._batch_size = batch_size
+        self._generator_update_ratio = generator_update_ratio
+        self._log_for_every = log_for_every
+        self._save_for_every = save_for_every
+        self._sample_dir = sample_dir
+        self._model_dir = model_dir
+
+        # model hyperparameters
         self._z_size = z_size
         self._image_size = image_size
-        self._lr = lr
+        self._learning_rate = learning_rate
         self._beta1 = beta1
 
         # placeholders
         self.z_in = z_in = tf.placeholder(
-            shape=[None, z_size], dtype=tf.float32,
+            shape=[None, z_size], dtype=tf.float32
         )
         self.image_in = image_in = tf.placeholder(
-            shape=[None, image_size, image_size, 1],
-            dtype=tf.float32,
+            shape=[None, image_size, image_size, 1], dtype=tf.float32
         )
 
         # build graph using a generator and a discriminator.
@@ -117,11 +132,11 @@ class DCGAN(object):
         g_loss = -tf.reduce_mean(tf.log(D_g))
 
         D_trainer = tf.train.AdamOptimizer(
-            learning_rate=self._lr,
+            learning_rate=self._learning_rate,
             beta1=self._beta1
         )
         G_trainer = tf.train.AdamOptimizer(
-            learning_rate=self._lr,
+            learning_rate=self._learning_rate,
             beta1=self._beta1,
         )
 
@@ -130,10 +145,78 @@ class DCGAN(object):
         g_grads = G_trainer.compute_gradients(g_loss, var_list=trainables[:9])
         update_D = D_trainer.apply_gradients(d_grads)
         update_G = G_trainer.apply_gradients(g_grads)
-
         return update_D, update_G, d_loss, g_loss
 
 
-    def train(self):
-        # TODO: NOT IMPLEMENTED YET
-        pass
+    def train(self, config, sess=None):
+        mnist = input_data.read_data_sets('MNIST/', one_hot=False)
+        saver = tf.train.Saver()
+        with sess or tf.Session() as sess:
+            try:
+                sess.run(tf.initialize_all_varaibles())
+            except:
+                sess.run(tf.global_variables_initializer())
+
+            for i in range(config.iterations):
+                # sample z from uniform distribution and prepare 
+                # fake/real images
+                zs = np.random.uniform(
+                    -1., 1., size=[config.batch_size, self._z_size]
+                ).astype(np.float32)
+                xs, _ = mnist.train.next_batch(config.batch_size)
+                xs = (np.reshape(xs, [config.batch_size, 28, 28, 1]) - 0.5) * 2.
+                xs = np.lib.pad(
+                    xs, ((0, 0), (2, 2), (2, 2), (0, 0)), 'constant', 
+                    constant_values=(-1, -1)
+                )
+
+                # run discriminator trainer
+                _, d_loss = sess.run(
+                    [self.update_D, self.d_loss], 
+                    feed_dict={
+                        self.z_in: zs, 
+                        self.image_in: xs
+                    }
+                )
+                # run generator trainer
+                for _ in range(config.generator_update_ratio):
+                    _, g_loss = sess.run(
+                        [self.update_G, self.g_loss], 
+                        feed_dict={self.z_in: zs}
+                    )
+
+                if i % config.log_for_every == 0:
+                    # log current training process status
+                    print('Generator Loss: {} / Discriminator Loss: {}'.format(
+                        g_loss, d_loss
+                    ))
+                    if not os.path.exists(config.sample_dir):
+                        os.makedirs(config.sample_dir, exist_ok=True)
+
+                    # generate images from the current generator
+                    z_sampled = np.random.uniform(
+                        -1., 1., size=[config.batch_size, self._z_size]
+                    ).astype(np.float32)
+
+                    x_generated = sess.run(
+                        self.G, feed_dict={self.z_in: z_sampled}
+                    )
+
+                    utils.save_images(
+                        np.reshape(
+                            x_generated[:config.sample_size], 
+                            [config.sample_size, 
+                             self._image_size, 
+                             self._image_size]
+                        ), 
+                        [config.sample_size // 6, 6], 
+                        '{}/fig{}.png'.format(config.sample_dir, i)
+                    )
+
+                # save the model
+                if i % config.save_for_every == 0 and i != 0:
+                    if not os.path.exists(config.model_dir):
+                        os.makedirs(config.model_dir, exist_ok=True)
+                    path = '{}/model-{}.cptk'.format(config.model_dir, i)
+                    saver.save(sess, path)
+                    print('saved model to {}'.format(path))
--- implementations/GAN/train.py
+++ implementations/GAN/train.py
@@ -1,93 +1,48 @@
 import os.path
 import numpy as np
 import tensorflow as tf
-from tensorflow.examples.tutorials.mnist import input_data
 from models import DCGAN
-import utils
+import pprint
 
 
+flags = tf.app.flags
+flags.DEFINE_integer('z_size', 100, 'size of latent code z [100]')
+flags.DEFINE_integer('image_size', 32, 'size of image [32]')
+flags.DEFINE_float('learning_rate', 0.0002, 'learning rate for Adam [0.0002]')
+flags.DEFINE_float('beta1', 0.5, 'momentum term of Adam [0.5]')
+flags.DEFINE_integer('iterations', 5000, 'training iteration number')
+flags.DEFINE_integer('batch_size', 128, 'training batch size')
+flags.DEFINE_integer('sample_size', 36, 'generator sample size')
+flags.DEFINE_integer(
+    'generator_update_ratio', 2, 
+    'number of updates for generator parameters per discriminator\'s updates'
+)
+flags.DEFINE_integer('log_for_every', 10, 'number of batches per logging')
+flags.DEFINE_integer(
+    'save_for_every', 1000, 'number of batches per saving the model'
+)
+flags.DEFINE_string('sample_dir', 'figures', 'directory of generated figures')
+flags.DEFINE_string('model_dir', 'models', 'directory of trained models')
+FLAGS = flags.FLAGS
 
-# ===============================
-# Model / Training Configurations
-# ===============================
 
-z_size = 100
-image_size = 32
-lr = 0.0002
-beta1 = 0.5
+def main(_):
+    pprint.PrettyPrinter().pprint(FLAGS.__flags)
+    dcgan = DCGAN(
+        z_size=FLAGS.z_size, 
+        image_size=FLAGS.image_size, 
+        learning_rate=FLAGS.learning_rate, 
+        beta1=FLAGS.beta1,
+        iterations=FLAGS.iterations, 
+        batch_size=FLAGS.batch_size,
+        generator_update_ratio=FLAGS.generator_update_ratio,
+        log_for_every=FLAGS.log_for_every,
+        save_for_every=FLAGS.save_for_every,
+        sample_dir=FLAGS.sample_dir,
+        model_dir=FLAGS.model_dir,
+    )
+    dcgan.train(FLAGS)
 
-iterations = 5000
-batch_size = 128
-sample_size = 36
-generator_loop_ratio = 2
-log_for_every = 10
-save_for_every = 1000
 
-sample_dir = './figures'
-model_dir = './models'
-
-
-def train(model, iterations=5000, batch_size=128,
-          sample_size = 36, generator_loop_ratio=2, 
-          log_for_every=10, save_for_every=1000):
-    mnist = input_data.read_data_sets('MNIST_data/', one_hot=False)
-    saver = tf.train.Saver()
-    with tf.Session() as sess:
-        try:
-            sess.run(tf.initialize_all_varaibles())
-        except:
-            sess.run(tf.global_variables_initializer())
-
-        for i in range(iterations):
-            # sample z from uniform distribution and prepare fake/real images
-            zs = np.random.uniform(
-                -1., 1., size=[batch_size, z_size]
-            ).astype(np.float32)
-            xs, _ = mnist.train.next_batch(batch_size)
-            xs = (np.reshape(xs, [batch_size, 28, 28, 1]) - 0.5) * 2.
-            xs = np.lib.pad(
-                xs, ((0, 0), (2, 2), (2, 2), (0, 0)), 'constant', 
-                constant_values=(-1, -1)
-            )
-
-            # run discriminator trainer
-            _, d_loss = sess.run(
-                [model.update_D, model.d_loss], 
-                feed_dict={
-                    model.z_in: zs, 
-                    model.image_in: xs
-                }
-            )
-
-            # run generator trainer
-            for _ in range(generator_loop_ratio):
-                _, g_loss = sess.run(
-                    [model.update_G, model.g_loss], 
-                    feed_dict={model.z_in: zs}
-                )
-
-            # log current training process status
-            if i % log_for_every == 0:
-                print('Generator Loss: {} / Discriminator Loss: {}'.format(
-                    g_loss, d_loss
-                ))
-
-                z_sampled = \
-                    np.random.uniform(-1., 1., size=[batch_size, z_size])\
-                    .astype(np.float32)
-                x_generated = sess.run(model.G, feed_dict={model.z_in: z_sampled})
-
-                if not os.path.exists(sample_dir):
-                    os.makedirs(sample_dir, exist_ok=True)
-
-                utils.save_images(np.reshape(
-                    x_generated[:sample_size], 
-                    [sample_size, image_size, image_size]
-                ), [sample_size // 6, 6], '{}/fig{}.png'.format(sample_dir, i))
-
-            # save the model
-            if i % save_for_every == 0 and i != 0:
-                if not os.path.exists(model_dir):
-                    os.mkdirs(model_dir, exist_ok=True)
-                saver.save(sess, '{}/model-{}.cptk'.format(model_dir, i))
-                print('saved model')
+if __name__ == '__main__':
+    tf.app.run()
