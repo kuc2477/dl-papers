a947a2372aa1419dd530b7caa1a3f32a267e7c22
Ha Junsoo <kuc2477@gmail.com>
2017-03-21 23:37:29 +0900

    fully working

--- GAN/DCGAN/main.py
+++ GAN/DCGAN/main.py
@@ -1,25 +1,37 @@
 import os.path
+import pprint
 import numpy as np
 import tensorflow as tf
 from model import DCGAN
-import pprint
+from train import train
 
 
 flags = tf.app.flags
 flags.DEFINE_integer('z_size', 100, 'size of latent code z [100]')
 flags.DEFINE_integer('image_size', 32, 'size of image [32]')
+flags.DEFINE_integer('channel_size', 1, 'size of channel [1]')
+flags.DEFINE_integer(
+    'g_filter_number', 64,
+    'number of generator\'s filters at the last transposed conv layer'
+)
+flags.DEFINE_integer(
+    'd_filter_number', 64,
+    'number of discriminator\'s filters at the first conv layer'
+)
+flags.DEFINE_integer('g_filter_size', 5, 'generator\'s filter size')
+flags.DEFINE_integer('d_filter_size', 4, 'discriminator\'s filter size')
 flags.DEFINE_float('learning_rate', 0.00002, 'learning rate for Adam [0.00002]')
 flags.DEFINE_float('beta1', 0.5, 'momentum term of Adam [0.5]')
 flags.DEFINE_integer('iterations', 5000, 'training iteration number')
 flags.DEFINE_integer('batch_size', 128, 'training batch size')
 flags.DEFINE_integer('sample_size', 36, 'generator sample size')
-flags.DEFINE_integer(
-    'generator_update_ratio', 2, 
-    'number of updates for generator parameters per discriminator\'s updates'
-)
 flags.DEFINE_integer('log_for_every', 10, 'number of batches per logging')
 flags.DEFINE_integer(
     'save_for_every', 1000, 'number of batches per saving the model'
+)
+flags.DEFINE_integer(
+    'generator_update_ratio', 2,
+    'number of updates for generator parameters per discriminator\'s updates'
 )
 flags.DEFINE_string('sample_dir', 'figures', 'directory of generated figures')
 flags.DEFINE_string('model_dir', 'checkpoints', 'directory of trained models')
@@ -28,8 +40,16 @@ FLAGS = flags.FLAGS
 
 def main(_):
     pprint.PrettyPrinter().pprint(FLAGS.__flags)
-    dcgan = DCGAN(z_size=FLAGS.z_size, image_size=FLAGS.image_size)
-    dcgan.train(FLAGS)
+    dcgan = DCGAN(
+        z_size=FLAGS.z_size,
+        image_size=FLAGS.image_size,
+        channel_size=FLAGS.channel_size,
+        g_filter_number=FLAGS.g_filter_number,
+        d_filter_number=FLAGS.d_filter_number,
+        g_filter_size=FLAGS.g_filter_size,
+        d_filter_size=FLAGS.d_filter_size,
+    )
+    train(dcgan, FLAGS)
 
 
 if __name__ == '__main__':
--- GAN/DCGAN/model.py
+++ GAN/DCGAN/model.py
@@ -1,144 +1,126 @@
-import abc
-import os.path
 import numpy as np
 import tensorflow as tf
 from tensorflow.contrib import slim
-from tensorflow.examples.tutorials.mnist import input_data
 from activations import lrelu
-import utils
 
 
-class GAN(metaclass=abc.ABCMeta):
-    def __init__(self, z_size, image_size):
-        # model-wise initializer 
+class DCGAN(object):
+    def __init__(
+        self,
+        z_size, image_size, channel_size,
+        g_filter_number, d_filter_number,
+        g_filter_size, d_filter_size,
+    ):
+        # model-wise initializer
         self._initialzier = tf.truncated_normal_initializer(stddev=0.002)
 
-        # basic hyperparameters and placeholders
-        self._z_size = z_size
-        self._z_in = z_in = tf.placeholder(
+        # basic hyperparameters
+        self.z_size = z_size
+        self.image_size = image_size
+        self.channel_size = channel_size
+        self.g_filter_number = g_filter_number
+        self.d_filter_number = d_filter_number
+        self.g_filter_size = g_filter_size
+        self.d_filter_size = d_filter_size
+
+        # basic placeholders
+        self.z_in = z_in = tf.placeholder(
             shape=[None, z_size], dtype=tf.float32
         )
-        self._image_size = image_size
-        self._image_in = image_in = tf.placeholder(
-            shape=[None, image_size, image_size, 1], dtype=tf.float32
+        self.image_in = image_in = tf.placeholder(
+            shape=[None, image_size, image_size, channel_size],
+            dtype=tf.float32,
         )
-    
-    @property
-    def z_size(self):
-        return self._z_size
 
-    @property
-    def z_in(self):
-        return self._z_in
-
-    @property
-    def image_size(self):
-        return self._image_size
-
-    @property
-    def image_in(self):
-        return self._image_in
-
-    @abc.abstractproperty
-    def G(self):
-        raise NotImplementedError
-
-    @abc.abstractproperty
-    def D_x(self):
-        raise NotImplementedError
-
-    @abc.abstractproperty
-    def D_g(self):
-        raise NotImplementedError
-
-    @abc.abstractproperty
-    def d_loss(self):
-        raise NotImplementedError
-
-    @abc.abstractproperty
-    def g_loss(self):
-        raise NotImplementedError
-
-    @abc.abstractmethod
-    def generator(self, z):
-        raise NotImplementedError
-
-    @abc.abstractmethod
-    def discriminator(self, bottom):
-        raise NotImplementedError
-
-
-class DCGAN(GAN):
-    def __init__(self, z_size, image_size):
-        super().__init__(z_size=z_size, image_size=image_size)
         # build graph using a generator and a discriminator.
-        self._G = self.generator(self.z_in)
-        self._D_x = self.discriminator(self.image_in)
-        self._D_g = self.discriminator(self.G, reuse=True)
-        self._d_loss = -tf.reduce_mean(tf.log(self.D_x) + tf.log(1. - self.D_g))
-        self._g_loss = -tf.reduce_mean(tf.log(self.D_g))
+        self.G = self.generator(self.z_in)
+        self.D_x, self.D_x_logits = self.discriminator(self.image_in)
+        self.D_g, self.D_g_logits = self.discriminator(self.G, reuse=True)
 
-    @property
-    def G(self):
-        return self._G
-
-    @property
-    def D_x(self):
-        return self._D_x
-
-    @property
-    def D_g(self):
-        return self._D_g
-
-    @property
-    def d_loss(self):
-        return self._d_loss
-
-    @property
-    def g_loss(self):
-        return self._g_loss
+        # build objective function
+        self.d_loss_real = tf.reduce_mean(
+            tf.nn.sigmoid_cross_entropy_with_logits(
+                logits=self.D_x_logits, labels=tf.ones_like(self.D_x)
+            )
+        )
+        self.d_loss_fake = tf.reduce_mean(
+            tf.nn.sigmoid_cross_entropy_with_logits(
+                logits=self.D_g_logits, labels=tf.zeros_like(self.D_g)
+            )
+        )
+        self.d_loss = self.d_loss_real + self.d_loss_fake
+        self.g_loss = tf.reduce_mean(
+            tf.nn.sigmoid_cross_entropy_with_logits(
+                logits=self.D_g_logits, labels=tf.ones_like(self.D_g)
+            )
+        )
+        self.g_vars = [v for v in tf.trainable_variables() if 'g_' in v.name]
+        self.d_vars = [v for v in tf.trainable_variables() if 'd_' in v.name]
 
     def generator(self, z):
+        # project z
+        z_filter_number = self.g_filter_number * 8
+        z_projection_size = self.image_size // 8
         z_projected = slim.fully_connected(
-            z, 4 * 4 * 256,
+            z,
+            z_projection_size * z_projection_size * z_filter_number,
             activation_fn=tf.nn.relu,
             normalizer_fn=slim.batch_norm,
             weights_initializer=self._initialzier,
             scope='g_projection',
         )
+        z_reshaped = tf.reshape(
+            z_projected,
+            [-1, z_projection_size, z_projection_size, z_filter_number]
+        )
 
-        z_reshaped = tf.reshape(z_projected, [-1, 4, 4, 256])
-
+        # transposed conv1
+        g_conv1_filter_number = z_filter_number // 2
         g_conv1 = slim.convolution2d_transpose(
-            z_reshaped, num_outputs=256,
-            kernel_size=[5, 5], stride=[2, 2], padding='SAME',
+            z_reshaped,
+            num_outputs=g_conv1_filter_number,
+            kernel_size=[self.g_filter_size, self.g_filter_size],
+            stride=[2, 2],
+            padding='SAME',
             activation_fn=tf.nn.relu,
             normalizer_fn=slim.batch_norm,
             weights_initializer=self._initialzier,
             scope='g_conv1',
         )
 
+        # transposed conv2
+        g_conv2_filter_number = g_conv1_filter_number // 2
         g_conv2 = slim.convolution2d_transpose(
-            g_conv1, num_outputs=64,
-            kernel_size=[5, 5], stride=[2, 2], padding='SAME',
+            g_conv1,
+            num_outputs=g_conv2_filter_number,
+            kernel_size=[self.g_filter_size, self.g_filter_size],
+            stride=[2, 2],
+            padding='SAME',
             activation_fn=tf.nn.relu,
             normalizer_fn=slim.batch_norm,
             weights_initializer=self._initialzier,
             scope='g_conv2',
         )
 
+        # transposed conv3
+        g_conv3_filter_number = g_conv2_filter_number // 2
         g_conv3 = slim.convolution2d_transpose(
-            g_conv2, num_outputs=16,
-            kernel_size=[5, 5], stride=[2, 2], padding='SAME',
+            g_conv2,
+            num_outputs=g_conv3_filter_number,
+            kernel_size=[self.g_filter_size, self.g_filter_size],
+            stride=[2, 2],
+            padding='SAME',
             activation_fn=tf.nn.relu,
             normalizer_fn=slim.batch_norm,
             weights_initializer=self._initialzier,
             scope='g_conv3',
         )
 
+        # out
         g_out = slim.convolution2d_transpose(
-            g_conv3, num_outputs=1,
-            kernel_size=[32, 32], padding='SAME',
+            g_conv3,
+            num_outputs=self.channel_size,
+            kernel_size=[self.image_size, self.image_size], padding='SAME',
             biases_initializer=None,
             activation_fn=tf.nn.tanh,
             weights_initializer=self._initialzier,
@@ -148,8 +130,12 @@ class DCGAN(GAN):
         return g_out
 
     def discriminator(self, bottom, reuse=False):
+        d_conv1_filter_number = self.d_filter_number
         d_conv1 = slim.convolution2d(
-            bottom, 16, kernel_size=[4, 4], stride=[2, 2], padding='SAME',
+            bottom,
+            d_conv1_filter_number,
+            kernel_size=[self.d_filter_size, self.d_filter_size],
+            stride=[2, 2], padding='SAME',
             activation_fn=lrelu,
             normalizer_fn=slim.batch_norm,
             biases_initializer=None,
@@ -157,8 +143,13 @@ class DCGAN(GAN):
             reuse=reuse, scope='d_conv1'
         )
 
+        d_conv2_filter_number = d_conv1_filter_number * 2
         d_conv2 = slim.convolution2d(
-            d_conv1, 64, kernel_size=[4, 4], stride=[2, 2], padding='SAME',
+            d_conv1,
+            d_conv2_filter_number,
+            kernel_size=[self.d_filter_size, self.d_filter_size],
+            stride=[2, 2],
+            padding='SAME',
             activation_fn=lrelu,
             normalizer_fn=slim.batch_norm,
             biases_initializer=None,
@@ -166,8 +157,13 @@ class DCGAN(GAN):
             reuse=reuse, scope='d_conv2'
         )
 
+        d_conv3_filter_number = d_conv2_filter_number * 2
         d_conv3 = slim.convolution2d(
-            d_conv2, 256, kernel_size=[4, 4], stride=[2, 2], padding='SAME',
+            d_conv2,
+            d_conv3_filter_number,
+            kernel_size=[self.d_filter_size, self.d_filter_size],
+            stride=[2, 2],
+            padding='SAME',
             activation_fn=lrelu,
             normalizer_fn=slim.batch_norm,
             biases_initializer=None,
@@ -178,103 +174,8 @@ class DCGAN(GAN):
         d_out = slim.fully_connected(
             slim.flatten(d_conv3), 1,
             weights_initializer=self._initialzier,
-            activation_fn=tf.nn.sigmoid,
             reuse=reuse,
             scope='d_out'
         )
 
-        return d_out
-
-    # TODO: MOVE THE TRAINING LOGIC TO train.py
-    def train(self, config, sess=None):
-        D_trainer = tf.train.AdamOptimizer(
-            learning_rate=config.learning_rate,
-            beta1=config.beta1
-        )
-        G_trainer = tf.train.AdamOptimizer(
-            learning_rate=config.learning_rate,
-            beta1=config.beta1,
-        )
-
-        trainables = tf.trainable_variables()
-        d_grads = D_trainer.compute_gradients(self.d_loss, var_list=trainables[9:])
-        g_grads = G_trainer.compute_gradients(self.g_loss, var_list=trainables[:9])
-        update_D = D_trainer.apply_gradients(d_grads)
-        update_G = G_trainer.apply_gradients(g_grads)
-
-        mnist = input_data.read_data_sets('MNIST/', one_hot=False)
-        saver = tf.train.Saver()
-        with sess or tf.Session() as sess:
-            try:
-                sess.run(tf.initialize_all_varaibles())
-            except:
-                sess.run(tf.global_variables_initializer())
-
-            for i in range(config.iterations):
-                # sample z from uniform distribution and prepare fake/real 
-                # images
-                zs = np.random.uniform(
-                    -1., 1., size=[config.batch_size, self._z_size]
-                ).astype(np.float32)
-                xs, _ = mnist.train.next_batch(config.batch_size)
-                xs = (np.reshape(xs, [config.batch_size, 28, 28, 1]) - 0.5) * 2.
-                xs = np.lib.pad(
-                    xs, ((0, 0), (2, 2), (2, 2), (0, 0)), 'constant', 
-                    constant_values=(-1, -1)
-                )
-
-                # run discriminator trainer
-                _, d_loss = sess.run(
-                    [update_D, self.d_loss], 
-                    feed_dict={
-                        self.z_in: zs, 
-                        self.image_in: xs
-                    }
-                )
-                # run generator trainer
-                for _ in range(config.generator_update_ratio):
-                    _, g_loss = sess.run(
-                        [update_G, self.g_loss], 
-                        feed_dict={self.z_in: zs}
-                    )
-
-                if i % config.log_for_every == 0:
-                    # log current training process status
-                    print('Generator Loss: {} / Discriminator Loss: {}'.format(
-                        g_loss, d_loss
-                    ))
-                    if not os.path.exists(config.sample_dir):
-                        os.makedirs(config.sample_dir, exist_ok=True)
-
-                    # generate images from the current generator
-                    z_sampled = np.random.uniform(
-                        -1., 1., size=[config.batch_size, self._z_size]
-                    ).astype(np.float32)
-
-                    x_generated = sess.run(
-                        self.G, feed_dict={self.z_in: z_sampled}
-                    )
-
-                    utils.save_images(
-                        np.reshape(
-                            x_generated[:config.sample_size], 
-                            [config.sample_size, 
-                             self._image_size, 
-                             self._image_size]
-                        ), 
-                        [config.sample_size // 6, 6], 
-                        '{}/fig{}.png'.format(config.sample_dir, i)
-                    )
-
-                # save the model
-                if i % config.save_for_every == 0 and i != 0:
-                    if not os.path.exists(config.model_dir):
-                        os.makedirs(config.model_dir, exist_ok=True)
-                    path = '{}/model-{}.cptk'.format(config.model_dir, i)
-                    saver.save(sess, path)
-                    print('saved model to {}'.format(path))
-
-
-class InfoGAN(GAN):
-    # TODO NOT IMPLEMENTED YET
-    pass
+        return d_out, tf.nn.sigmoid(d_out)
