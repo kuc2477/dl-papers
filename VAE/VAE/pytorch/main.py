b682f57e50895d543329014bc32cecb410ca02f2
Ha Junsoo <kuc2477@gmail.com>
2017-06-18 18:50:15 +0900

    implement overall logics except for deconv

--- VAE/VAE/pytorch/main.py
+++ VAE/VAE/pytorch/main.py
@@ -9,6 +9,7 @@ parser.add_argument('--image-size', type=int, default=64)
 parser.add_argument('--channel-num', type=int, default=3)
 parser.add_argument('--kernel-num', type=int, default=128)
 parser.add_argument('--kernel-size', type=int, default=4)
+parser.add_argument('--latent-code-size', type=int, default=128)
 parser.add_argument('-b', '--batch-size', type=int, default=32)
 parser.add_argument('-e', '--epoch', type=int, default=10)
 parser.add_argument('-l', '--lr', type=float, default=0.01)
@@ -24,5 +25,7 @@ if __name__ == '__main__':
         image_size=args.image_size,
         channel_num=args.channel_num,
         kernel_num=args.kernel_num,
-        kernel_size=args.kernel_size
+        kernel_size=args.kernel_size,
+        latent_code_size=args.latent_code_size,
+        use_cuda=args.cuda,
     )
--- VAE/VAE/pytorch/model.py
+++ VAE/VAE/pytorch/model.py
@@ -1,3 +1,4 @@
+import torch
 from torch import nn
 
 
@@ -10,40 +11,54 @@ def _pad_transposed_conv_same(kernel_size):
 
 
 class VAE(nn.Module):
-    def __init__(self, image_size, channel_num, kernel_num, kernel_size):
+    def __init__(self, image_size, channel_num,
+                 kernel_num, kernel_size, latent_code_size, use_cuda):
         self.image_size = image_size
         self.channel_num = channel_num
         self.kernel_num = kernel_num
         self.kernel_size = kernel_size
+        self.latent_code_size = latent_code_size
+        self.use_cuda = use_cuda
 
         # encoder
         self.encoder = nn.Sequential(
-            self._conv(self.channel_num, kernel_num // 8),
-            self._conv(kernel_num // 8, kernel_num // 4),
+            self._conv(channel_num, kernel_num // 4),
             self._conv(kernel_num // 4, kernel_num // 2),
             self._conv(kernel_num // 2, kernel_num),
         )
 
         # q
-        self.q_mu = self._linear()
-        self.q_var = self._linear()
+        self.q_mu = self._linear(
+            kernel_num * (image_size // 8) ** 2,
+            latent_code_size
+        )
+        self.q_logvar = self._linear(
+            kernel_num * (image_size // 8) ** 2,
+            latent_code_size,
+        )
 
         # decoder
         self.decoder = nn.Sequential(
-            self._deconv(self.channel_num, kernel_num),
             self._deconv(kernel_num, kernel_num // 2),
             self._deconv(kernel_num // 2, kernel_num // 4),
-            self._deconv(kernel_num // 4, kernel_num // 8),
+            self._deconv(kernel_num // 4, channel_num),
         )
 
     def forward(self, x):
         encoded = self.encoder(x)
-        mu, var = self.q_mu(encoded), self.q_var(encoded)
-        z = self._sample(mu, var)
-        return self.decoder(z)
+        mu, logvar = self.q_mu(encoded), self.q_logvar(encoded)
+        z = self._sample(mu, logvar)
+        z_projected = self._project(z)
+        return self.decoder(z_projected)
 
-    def _sample(self, mu, var):
-        pass
+    def _sample(self, mu, logvar):
+        std = logvar.mul(0.5).exp_()
+        eps = torch.Variable(
+            (torch.cuda if self.use_cuda else torch)
+            .FloatTensor(std.size())
+            .normal_()
+        )
+        return eps.mul(std).add_(mu)
 
     def _conv(self, channel_size, kernel_num):
         return nn.Sequential(
@@ -57,6 +72,7 @@ class VAE(nn.Module):
         )
 
     def _deconv(self, channel_num, kernel_num):
+        # TODO: NOT IMPLEMENTED YET
         return nn.Sequential(
             nn.ConvTranspose2d(
                 channel_num, kernel_num,
@@ -65,6 +81,8 @@ class VAE(nn.Module):
             )
         )
 
-    def _linear(self):
+    def _linear(self, in_size, out_size):
         return nn.Sequential(
+            nn.Linear(in_size, out_size),
+            nn.ReLU(),
         )
