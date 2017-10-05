059ba5c609eb24fc74755e76f6f7f0987d73883a
Ha Junsoo <kuc2477@gmail.com>
2017-10-06 02:31:07 +0900

    add memory class skeletons

--- Memory/MemN2N/pytorch/model.py
+++ Memory/MemN2N/pytorch/model.py
@@ -1,13 +1,69 @@
+from torch import Tensor
 from torch import nn
+from torch.autograd import Variable
+
+
+class AbstractMemory(nn.Module):
+    def __init__(self,
+                 vocabulary_size, sentence_size,
+                 embedding_size, memory_size,
+                 embedding=None):
+        self.embedding_size = embedding_size
+        self.memory_size = memory_size
+
+        self.softmax = nn.Softmax()
+        self.embedding = embedding or nn.Embedding(
+            vocabulary_size, embedding_size
+        )
+        self.memory = Variable(Tensor(
+            memory_size, embedding_size
+        ).normal_(std=.1))
+
+
+class InputMemory(AbstractMemory):
+    def forward(self, x, query):
+        embedded_x = self.embedding(x)
+        embedded_query = self.embedding(query)
+        return self.softmax(embedded_query.dot(embedded_x))
+
+
+class OutputMemory(AbstractMemory):
+    def forward(self, x, p):
+        embedded_x = self.embedding(x)
+        return (p * embedded_x).sum()
 
 
 class MemN2N(nn.Module):
-    def __init__(self, vocabulary_size, embedding_size, memory_size, hops):
+    def __init__(self,
+                 vocabulary_size,
+                 sentence_size,
+                 embedding_size,
+                 memory_size, hops):
         super().__init__()
         self.vocabulary_size = vocabulary_size
+        self.sentence_size = sentence_size
         self.embedding_size = embedding_size
         self.memory_size = memory_size
         self.hops = hops
 
-    def forward(x):
-        pass
+        self.linear = nn.Linear(self.vocabulary_size, self.embedding_size)
+        self.softmax = nn.Softmax()
+        self.A = InputMemory(
+            self.vocabulary_size,
+            self.sentence_size,
+            self.embedding_size,
+            self.memory_size
+        )
+
+        self.C = OutputMemory(
+            self.vocabulary_size,
+            self.sentence_size,
+            self.embedding_size,
+            self.memory_size
+        )
+
+    def forward(self, x, query):
+        u = self.A.embedding(query)
+        p = self.A(x, u)
+        o = self.C(x, p)
+        return self.softmax(self.linear(o + u))
