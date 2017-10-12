4fb61d075ed22d9197148d2a712e865ca8f89692
Ha Junsoo <kuc2477@gmail.com>
2017-10-12 22:08:44 +0900

    add tests. embedding_size -> memory_feature_size.

--- Memory/NTM/pytorch/model.py
+++ Memory/NTM/pytorch/model.py
@@ -1,118 +1,265 @@
+import abc
+import operator as op
+from functools import reduce
+import torch
+from torch import Tensor
+from torch.autograd import Variable
 from torch import nn
+from torch.nn import functional as F
+from const import EPSILON
 
 
-class StatefulComponent(nn.Module):
-    def reset(self):
-        raise NotImplementedError
+class StatefulComponent(nn.Module, metaclass=abc.ABCMeta):
+    def __init__(self):
+        super().__init__()
+        self._expected_batch_size = None
 
+    @abc.abstractmethod
+    def reset(self, batch_size):
+        """
+        Resets the state of the component. Note that all initialized
+        state variables must fit into the given batch size.
+        """
+        self._expected_batch_size = batch_size
+
+    @property
+    def expected_batch_size(self):
+        return self._expected_batch_size
+
+
+# ==============
+# NTM Components
+# ==============
 
 class Controller(StatefulComponent):
-    def __init__(self):
-        # TODO: NOT IMPLEMENTED YET
-        self.embedding = nn.Embedding()
-        self.cell = nn.LSTMCell()
-        pass
+    def __init__(self, dictionary_size, embedding_size, hidden_size):
+        # Configurations.
+        super().__init__()
+        self.dictionary_size = dictionary_size
+        self.embedding_size = embedding_size
+        self.hidden_size = hidden_size
+
+        # Layers.
+        self.embedding = nn.Embedding(
+            self.dictionary_size,
+            self.embedding_size
+        )
+        self.cell = nn.LSTMCell(self.embedding_size, self.hidden_size)
+        self.h = None
+        self.c = None
 
     def forward(self, x):
-        # TODO: NOT IMPLEMENTED YET
-        pass
+        self.h, self.c = self.cell(self.embedding(x), (self.h, self.c))
+        return self.h
 
-    def reset(self):
-        # TODO: NOT IMPLEMENTED YET
-        pass
+    def reset(self, batch_size):
+        super().reset(batch_size)
+        self.h = Variable(Tensor(batch_size, self.hidden_size))
+        self.c = Variable(Tensor(batch_size, self.hidden_size))
 
 
 class Head(StatefulComponent):
-    def __init__(self):
-        # TODO: NOT IMPLEMENTED YET
-        pass
+    def __init__(self,
+                 hidden_size,
+                 memory_size,
+                 memory_feature_size,
+                 max_shift_size):
+        # Configurations
+        super().__init__()
+        self.hidden_size = hidden_size
+        self.memory_size = memory_size
+        self.memory_feature_size = memory_feature_size
+        self.max_shift_size = max_shift_size
 
-    def forward(self, embedded, m):
+        # Interprets a hidden state passed from a controller
+        self.interpreter = nn.Linear(
+            self.hidden_size,
+            sum([s for s, _ in self.interpreter_splitting_scheme()])
+        )
+
+        # Current head position
+        self.w = None
+
+    @abc.abstractmethod
+    def forward(self, h, m):
         raise NotImplementedError
 
-    def reset(self):
-        # TODO: NOT IMPLEMENTED YET
-        pass
+    @abc.abstractmethod
+    def interpreter_splitting_scheme(self):
+        raise NotImplementedError
 
-    def _move(self, m, k, b, g, s, r):
-        w = self._find_by_content_addressing(m, k)
-        w = self._interpolate_with_previous_head(w, g)
-        w = self._move_by_location_addressing(w, s)
+    def interpret(self, h):
+        __import__('pdb').set_trace()
+        interpreted = self.interpreter(h)
+        splits, activations = zip(*self.interpreter_splitting_scheme())
+        splits = [
+            reduce(op.add, [0, *splits][:i+1]) for i in
+            range(len(splits)+1)
+        ]
+        return tuple(
+            activation(interpreted[:, s:e]) for s, e, activation in
+            zip(splits[:-1], splits[1:], activations)
+        )
+
+    def move(self, m, k, b, g, s, r):
+        w = self._find_by_content_addressing(m, k, b)
+        w = self._interpolate_with_current_position(w, g)
+        w = self._shift_by_location_addressing(w, s)
         w = self.w = self._sharpen(w, r)
         return w
 
-    def _interpret(self, embedded):
-        raise NotImplementedError
+    def reset(self, batch_size):
+        super().reset(batch_size)
+        self.w = Variable(Tensor(batch_size, self.memory_size))
 
-    def _interpolate_with_previous_head(self, w, g):
-        # TODO: NOT IMPLEMENTED YET
-        pass
+    def _interpolate_with_current_position(self, w, g):
+        return w + (1-g)*self.w
 
-    def _find_by_content_addressing(self, m, k):
-        # TODO: NOT IMPLEMENTED YET
-        pass
+    def _find_by_content_addressing(self, m, k, b):
+        entire_memory_size = self.expected_batch_size * self.memory_size
+        m_unrolled = m.view(entire_memory_size, -1)
+        k_unrolled = k.unsqueeze(1).expand_as(m).view(entire_memory_size, -1)
 
-    def _move_by_location_addressing(self, w, s):
-        # TODO: NOT IMPLEMENTED YET
-        pass
+        similarity_between_key_and_memories = F\
+            .cosine_similarity(m_unrolled + EPSILON, k_unrolled + EPSILON)\
+            .view(-1, self.memory_size)
+
+        return F.softmax(b * similarity_between_key_and_memories)
+
+    def _shift_by_location_addressing(self, w, s):
+        shifted = Variable(Tensor(*w.size()))
+        for b in range(self.expected_batch_size):
+            wb, sb = w[b], s[b]
+            # unroll the head positions so that we can convolve with.
+            wb_modulo_unrolled = torch.cat(
+                wb[-self.max_shift_size:],
+                wb,
+                wb[:self.max_shift_size]
+            )
+            # convolve head positions with the shifts.
+            shifted[b, :] = F.conv1d(
+                wb_modulo_unrolled.view(1, 1, -1),
+                sb.view(1, 1, -1)
+            ).view(-1)
+        return shifted
 
     def _sharpen(self, w, r):
-        # TODO: NOT IMPLEMENTED YET
-        pass
+        return (w**r) / ((w**r).sum(1).view(-1, 1) + EPSILON)
 
 
 class ReadHead(Head):
-    def forward(self, embedded, m):
-        k, b, g, s, r = self._interpret(embedded)
-        return self._move(m, k, b, g, s, r)
+    def forward(self, h, m):
+        k, b, g, s, r = self.interpret(h)
+        return self.move(m, k, b, g, s, r)
 
-    def _interpret(self, embedded):
-        # TODO: NOT IMPLEMENTED YET
-        pass
+    def interpreter_splitting_scheme(self):
+        return [
+            (self.memory_feature_size, lambda x: x),     # k
+            (1, F.softplus),                        # b
+            (1, F.sigmoid),                         # g
+            (2*self.max_shift_size+1, F.softmax),   # s
+            (1, lambda x: 1 + F.softplus(x))        # r
+        ]
 
 
 class WriteHead(Head):
-    def forward(self, embedded, m):
-        k, b, g, s, r, e, a = self._interpret(embedded)
-        return self._move(m, k, b, g, s, r), e, a
+    def forward(self, h, m):
+        k, b, g, s, r, e, a = self.interpret(h)
+        return self.move(m, k, b, g, s, r), e, a
 
-    def _interpret(self, embedded):
-        # TODO: NOT IMPLEMENTED YET
-        pass
+    def interpreter_splitting_scheme(self):
+        return [
+            (self.memory_feature_size, lambda x: x),     # k
+            (1, F.softplus),                        # b
+            (1, F.sigmoid),                         # g
+            (2*self.max_shift_size+1, F.softmax),   # s
+            (1, lambda x: 1 + F.softplus(x))        # r,
+            (self.memory_feature_size, F.sigmoid)        # e
+            (self.memory_feature_size, lambda x: x)      # a
+        ]
 
 
 class Memory(StatefulComponent):
+    def __init__(self, memory_size, memory_feature_size):
+        # Configurations.
+        super().__init__()
+        self.memory_size = memory_size
+        self.memory_feature_size = memory_feature_size
+
+        # Memory bank.
+        self.bank = None
+
     def read(self, w):
-        # TODO: NOT IMPLEMENTED YET
-        pass
+        return (w * self.bank).sum(1)
 
     def write(self, w, e, a):
-        # TODO: NOT IMPLEMENTED YET
-        pass
+        self.bank = self.bank * (1 - w*e)
+        self.bank = self.bank + w*a
 
-    def reset(self):
-        # TODO: NOT IMPLEMENTED YET
-        pass
+    def reset(self, batch_size):
+        super().reset(batch_size)
+        self.bank = Variable(Tensor(
+            batch_size,
+            self.memory_size,
+            self.memory_feature_size,
+        ))
 
 
 class NTM(StatefulComponent):
-    def __init__(self):
-        # TODO: NOT IMPLEMENTED YET
+    def __init__(self,
+                 dictionary_size, embedding_size, hidden_size,
+                 memory_size, memory_feature_size, max_shift_size):
+        # Configurations.
         super().__init__()
-        self.controller = Controller()
-        self.read_head = ReadHead()
-        self.write_head = WriteHead()
-        self.memory = Memory()
-        self.linear = nn.Linear()
+        self.dictionary_size = dictionary_size
+        self.embedding_size = embedding_size
+        self.hidden_size = hidden_size
+        self.memory_size = memory_size
+        self.memory_feature_size = memory_feature_size
+        self.max_shift_size = max_shift_size
+
+        # Components.
+        self.controller = Controller(
+            self.dictionary_size,
+            self.embedding_size,
+            self.hidden_size,
+        )
+        self.read_head = ReadHead(
+            self.hidden_size,
+            self.memory_size,
+            self.memory_feature_size,
+            self.max_shift_size,
+        )
+        self.write_head = WriteHead(
+            self.hidden_size,
+            self.memory_size,
+            self.memory_feature_size,
+            self.max_shift_size,
+        )
+        self.memory = Memory(self.memory_size, self.memory_feature_size)
+        self.linear = nn.Linear(self.memory_feature_size, self.dictionary_size)
 
     def forward(self, x=None, return_read_memory=False):
-        embedded = self.controller(x)
-        self.memory.write(*self.write_head(embedded, self.memory))
-        r = self.memory.read(*self.read_head(embedded, self.memory))
+        if self.expected_batch_size is None:
+            raise RuntimeError(
+                'NTM tape is not ready. '
+                'You need to initialize the NTM tape before running it.'
+            )
+
+        if x is not None and x.size(0) != self.expected_batch_size:
+            raise RuntimeError((
+                'Input batch size and NTM tape size does not match. '
+                'Given batch is size of {}. Batch size {} is expected.'
+            ).format(x.size(0), self.expected_batch_size))
+
+        h = self.controller(x)
+        self.memory.write(*self.write_head(h, self.memory.bank))
+        r = self.memory.read(*self.read_head(h, self.memory.bank))
         return r if return_read_memory else self.linear(r)
 
-    def reset(self):
-        self.controller.reset()
-        self.read_head.reset()
-        self.write_head.reset()
-        self.memory.reset()
+    def reset(self, batch_size):
+        super().reset(batch_size)
+        self.controller.reset(batch_size)
+        self.read_head.reset(batch_size)
+        self.write_head.reset(batch_size)
+        self.memory.reset(batch_size)
---
+++ Memory/NTM/pytorch/tests.py
@@ -0,0 +1,97 @@
+from random import randint
+import pytest
+import torch
+from torch.autograd import Variable
+from model import Controller, ReadHead, WriteHead, Memory, NTM
+
+
+# ============================
+# Model Configuration Fixtures
+# ============================
+
+@pytest.fixture
+def batch_size():
+    return 32
+
+
+@pytest.fixture
+def dictionary_size():
+    return 30
+
+
+@pytest.fixture
+def input_length():
+    return 20
+
+
+@pytest.fixture
+def embedding_size():
+    return 10
+
+
+@pytest.fixture
+def memory_size():
+    return 30
+
+
+@pytest.fixture
+def hidden_size():
+    return 15
+
+
+@pytest.fixture
+def max_shift_size():
+    return 2
+
+
+@pytest.fixture
+def batch(batch_size, dictionary_size, input_length):
+    samples = []
+    for _ in range(batch_size):
+        sequence = [randint(0, dictionary_size-1) for _ in range(input_length)]
+        samples.append(sequence)
+    return Variable(torch.Tensor(samples).long())
+
+
+# ========================
+# Model Component Fixtures
+# ========================
+
+@pytest.fixture
+def controller(dictionary_size, embedding_size, hidden_size):
+    return Controller(dictionary_size, embedding_size, hidden_size)
+
+
+@pytest.fixture
+def read_head(hidden_size, memory_size, max_shift_size):
+    return ReadHead(hidden_size, memory_size, max_shift_size)
+
+
+# =====================
+# Model Component Tests
+# =====================
+
+def test_controller(controller, input_length, hidden_size, batch_size, batch):
+    assert batch.size() == (batch_size, input_length)
+    assert controller.h is None and controller.c is None
+    controller.reset(batch_size)
+    assert controller.expected_batch_size == batch_size
+    assert controller.h is not None and controller.c is not None
+    assert controller(batch[:, 0]).size() == (batch_size, hidden_size)
+
+
+def test_read_head(controller, read_head, batch_size, batch):
+    assert read_head.w is None
+    controller.reset(batch_size)
+    read_head.reset(batch_size)
+    assert read_head.w is not None
+    embedded = controller(batch[:, 0])
+    read_head.interpret(embedded)
+
+
+def test_write_head():
+    pass
+
+
+def test_memory():
+    pass
